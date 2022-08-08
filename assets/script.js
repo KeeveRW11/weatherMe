8f2ef6cb6f93a7a381560e949fcbe861abd02fc7
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-08 16:43:13 -0400

    search linked

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
-var getWeatherInfo = function() {
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=b8d240244c4cb961083733683eedcad3");
+var citySearchEl = document.querySelector("#city-search");
+var cityNameEl = document.querySelector("#city-name");
+
+getWeatherInfo = function() {
+   var apiUrl = ("https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=b8d240244c4cb961083733683eedcad3")
+   fetch(apiUrl).then(function(response){
+    response.json()
+    then(function(data){
+    console.log(data);
+});
+});
 };
 
-getWeatherInfo();//test
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+  };
+
+  citySearchEl.addEventListener("submit", formSubmitHandler);

40188215453255cee0376d61d3fd7b37b6292580
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-09 14:45:42 -0400

    double fetch work

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var cityFormEl = document.querySelector("#city-search");
 var cityInputEl = document.querySelector("#city-name");
 
-var apiKey = "&limit=5&appid=b8d240244c4cb961083733683eedcad3";
+var apiKey = "5&appid=b8d240244c4cb961083733683eedcad3";
 var cityCoords = "http://api.openweathermap.org/geo/1.0/direct?q=";
 var openWeatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat="
 
@@ -21,16 +21,17 @@ var formSubmitHandler = function(event) {
                
 //get the city coorddinates to plug into the one call api below
 var getCityInfo = function(name) {
-  var apiUrl = cityCoords + name + apiKey;
+  var apiUrl = cityCoords + name + "&limit=5" + apiKey;
     fetch(apiUrl)
         .then(function(response) {
             if(response.ok) {
                 response.json().then(function(data) {
-                    var cityLat = data.lat;
-                    var cityLon = data.lon;
-                    console.log = data
+                    var cityLat = data[0].lat;
+                    console.log(cityLat);
+                    var cityLon = data[0].lon;
+                    console.log(cityLon);
                     //"https://api.openweathermap.org/data/2.5/onecall?lat="
-                    var weatherApiUrl = openWeatherApi + cityLat + "&lon=" + cityLon + "&appid=" + apiKey;
+                    var weatherApiUrl = openWeatherApi + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly,alerts" + apiKey;
                     fetch(weatherApiUrl)
                       .then(function(response) {
                         if (response.ok) {

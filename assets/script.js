b3be0893506306fad5a95abe89b9217dc3260cea
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-11 08:58:51 -0400

    try local storage

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var cityFormEl = document.querySelector("#city-search");
 var cityInputEl = document.querySelector("#city-name");
+var cityInputEl = document.getElementById("city-name");
 
 var apiKey = "5&appid=b8d240244c4cb961083733683eedcad3";
 var cityCoords = "http://api.openweathermap.org/geo/1.0/direct?q=";
@@ -11,22 +12,19 @@ var currentDay = moment().format('M/DD/YYYY');
 //get the city coorddinates to plug into the one call api below
 var getCityInfo = function(cityName) {
   var apiUrl = cityCoords + cityName + "&limit=5" + apiKey;
-    fetch(apiUrl)
-        .then(function(response) {
-            if(response.ok) {
-                response.json().then(function(data) {
-                    var cityLat = data[0].lat;
-                    console.log(cityLat);
-                    var cityLon = data[0].lon;
-                    console.log(cityLon);
-                    //"https://api.openweathermap.org/data/2.5/onecall?lat="
-                    var weatherApiUrl = openWeatherApi + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly,alerts" + apiKey;
-                    fetch(weatherApiUrl)
-                      .then(function(response) {
-                        if (response.ok) {
-                            response.json().then(function(oneCallData) {
-                            console.log(oneCallData);
-                                           
+    fetch(apiUrl).then(function(response) {
+    if(response.ok) {
+     response.json().then(function(data) {
+        var cityLat = data[0].lat;
+          console.log(cityLat);
+            var cityLon = data[0].lon;
+                console.log(cityLon);
+                //"https://api.openweathermap.org/data/2.5/onecall?lat="
+                var weatherApiUrl = openWeatherApi + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly,alerts" + apiKey;
+                fetch(weatherApiUrl).then(function(response) {
+                if (response.ok) {
+                    response.json().then(function(oneCallData) {
+                        console.log(oneCallData);                           
                         });
                     };
                 });
@@ -52,17 +50,15 @@ var formSubmitHandler = function(event) {
     }
 };
 
-function save(){
-    var newData = document.getElementById("city-name").value;
-    if(localStorage.getItem("data") == null) {
-        localStorage.setItem("data", "[]");
-    }
-    var old_data = JSON.parse(localStorage.getItem("data"));
-    old_data.push(newData);
-    localStorage.setItem("data", JSON.stringify(old_data))
-};
+// save search history to load on next visit to website
+function saveSearchHistory(cities) {
+    searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-window.document.querySelector("button");
-console.dir("btn");
+    if (!searchHistory.includes(cities)) {
+        searchHistory.push(cities);
+    }
+
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+};
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
